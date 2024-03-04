15724b86b2494d2cb904b9b1709485d15099af67
mary-brooks <maryemmabrooks@gmail.com>
2024-03-04 19:59:49 +0000

    displayed budget summary information

--- src/pages/budgets/AllBudgetsPage.jsx
+++ src/pages/budgets/AllBudgetsPage.jsx
@@ -1,9 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 
 import { getAllBudgets } from '../../api/budgets.api';
+import { AuthContext } from '../../context/auth.context';
 
 function AllBudgetsPage() {
+  const { user } = useContext(AuthContext);
   const [budgets, setBudgets] = useState([]);
+
+  // Helper function to format the date
+  const formatDate = dateString => {
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   const getBudgets = async () => {
     try {
@@ -19,7 +28,29 @@ function AllBudgetsPage() {
     getBudgets();
   }, []);
 
-  return <div>AllBudgetsPage</div>;
+  return (
+    <Flex align='center' direction='column' padding={4} m={2}>
+      {user && (
+        <Heading size='lg' color='green.500'>
+          {user.name}'s Budgets
+        </Heading>
+      )}
+
+      {budgets &&
+        budgets.map(budget => {
+          return (
+            <div key={budget._id}>
+              <Text>{budget.name}</Text>
+              <Text>{`${formatDate(budget.startDate)} - ${formatDate(
+                budget.endDate
+              )}`}</Text>
+              <Text>{`Total Income: ${budget.totalIncome}`}</Text>
+              <Text>{`Savings Goal: ${budget.savingsGoal}`}</Text>
+            </div>
+          );
+        })}
+    </Flex>
+  );
 }
 
 export default AllBudgetsPage;
