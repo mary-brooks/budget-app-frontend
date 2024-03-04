9fe917d8f2cc1b24dcd7b7f6dc6a6faed8d49cff
mary-brooks <maryemmabrooks@gmail.com>
2024-03-04 19:25:37 +0000

    implemented getBudgets for AllBudgets page

--- src/pages/budgets/AllBudgetsPage.jsx
+++ src/pages/budgets/AllBudgetsPage.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react';
+
+import { getAllBudgets } from '../../api/budgets.api';
+
 function AllBudgetsPage() {
+  const [budgets, setBudgets] = useState([]);
+
+  const getBudgets = async () => {
+    try {
+      const response = await getAllBudgets();
+      console.log(response);
+      setBudgets(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBudgets();
+  }, []);
+
   return <div>AllBudgetsPage</div>;
 }
 
