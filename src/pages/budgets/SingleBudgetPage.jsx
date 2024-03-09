452c7ff6fcb095d965ae3b7bcbb213c919bfb66c
mary-brooks <maryemmabrooks@gmail.com>
2024-03-09 10:52:21 +0000

    retrieved single budget details & added edit budget button

--- src/pages/budgets/SingleBudgetPage.jsx
+++ src/pages/budgets/SingleBudgetPage.jsx
@@ -1,5 +1,40 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Button } from '@chakra-ui/react';
+import { getBudget } from '../../api/budgets.api';
+import { useParams } from 'react-router-dom';
+
 function SingleBudgetPage() {
-  return <div>SingleBudgetPage</div>;
+  const { budgetId } = useParams();
+
+  const [budget, setBudget] = useState();
+
+  const getSingleBudget = async () => {
+    try {
+      const response = await getBudget(budgetId);
+      console.log(response);
+      setBudget(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSingleBudget();
+  }, []);
+
+  return (
+    <>
+      <div>SingleBudgetPage</div>
+      {budget && (
+        <Link to={`/budgets/update/${budget._id}`}>
+          <Button colorScheme='green' variant='outline'>
+            Edit Budget
+          </Button>
+        </Link>
+      )}
+    </>
+  );
 }
 
 export default SingleBudgetPage;
