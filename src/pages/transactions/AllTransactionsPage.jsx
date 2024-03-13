4ed53a2ca9a57a25e34471046755e2921714859a
mary-brooks <maryemmabrooks@gmail.com>
2024-03-13 20:27:01 +0000

    retrieved all transactions

--- src/pages/transactions/AllTransactionsPage.jsx
+++ src/pages/transactions/AllTransactionsPage.jsx
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getAllTransactions } from '../../api/transactions.api';
+
 function AllTransactionsPage() {
-  return <div>AllTransactionsPage</div>;
+  const { budgetId } = useParams();
+
+  const [transactions, setTransactions] = useState('');
+
+  const getTransactions = async () => {
+    try {
+      const response = await getAllTransactions(budgetId);
+      console.log(response);
+      setTransactions(response.data);
+    } catch (error) {
+      console.log('Error retrieving transactions', error);
+    }
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
+  return <div></div>;
 }
 
 export default AllTransactionsPage;
