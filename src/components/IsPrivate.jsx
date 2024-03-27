41604fa2df50c235e001d6f8bb2cd293ff1d9532
mary-brooks <maryemmabrooks@gmail.com>
2024-03-27 22:38:17 +0000

    added loading spinner

--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,13 +1,14 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/auth.context';
 import { Navigate } from 'react-router-dom';
+import LoadingSpinner from './LoadingSpinner';
 
 function IsPrivate(props) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
   // if the authentication is still loading
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <LoadingSpinner />;
   }
 
   if (!isLoggedIn) {
